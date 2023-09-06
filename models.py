dd835d25ac257a0a4045f5978a4aee26cdc2d4de
Wandati <Petersimon801@gmail.com>
2023-09-06 22:46:35 +0300

    update

--- models.py
+++ models.py
@@ -21,55 +21,55 @@ class Customer(Base):
     accounts = relationship('Account',back_populates='users',cascade='all, delete')
     associated_banks = relationship('Bank',secondary=customer_bank_association,back_populates='users')
     
-    def open_account(self,initial_balance):
-        customer = session.query(Customer).filter_by(id=self.id).first()
-        if customer:
-            if initial_balance > 500:
-                new_account = Account(number= random.randint(1000000,6000000),amount=initial_balance)
-                customer.accounts.append(new_account)
-                session.commit()
+    # def open_account(self,initial_balance):
+    #     customer = session.query(Customer).filter_by(id=self.id).first()
+    #     if customer:
+    #         if initial_balance > 500:
+    #             new_account = Account(number= random.randint(1000000,6000000),amount=initial_balance)
+    #             customer.accounts.append(new_account)
+    #             session.commit()
                 
-                # session.add(new_account)
-                # session.commit()
-            else:
-                print("deposit cannot be less than 500!!")
-        else:
-            print("Customer not Found!!!")
-        # session.add(new_account)
-        # session.commit()
-        # return new_account
-    def make_deposit(self,ac_number,amount):
-        account = session.query(Account).filter_by(number=ac_number).first()
-        if account in self.accounts:
-            account.amount += amount
-        else:
-            print("Invalid Account!!")
+    #             # session.add(new_account)
+    #             # session.commit()
+    #         else:
+    #             print("deposit cannot be less than 500!!")
+    #     else:
+    #         print("Customer not Found!!!")
+    #     # session.add(new_account)
+    #     # session.commit()
+    #     # return new_account
+    # def make_deposit(self,ac_number,amount):
+    #     account = session.query(Account).filter_by(number=ac_number).first()
+    #     if account in self.accounts:
+    #         account.amount += amount
+    #     else:
+    #         print("Invalid Account!!")
        
             
-        # if ac_number == Account.number:
-        #     Account.amount += amount
-        # else:
-        #     print("Invalid Account!!!")
-        # if ac_number :
-        #     self.accounts.amount += amount
-        # else:
-        #     print("invalid Account!!")
-        # for account in self.accounts:
-        #     if account.number == ac_number:
-        #         account.amount += amount
-        #         new_balance = account.amount
-        #     else:
-        #         print("Invalid Account Number!!")
-        # return new_balance
-    def make_withdrawal(self,ac_number,amount):
-        account = session.query(Account).filter_by(number=ac_number).first()
-        if account in self.accounts:
-            if amount > 0 and  amount < account.amount:
-                account.amount -= amount
-            else:
-                print("Withdrawal cannot be less than 0 or  more than the remaining balance")
-        else:
-            print("Invalid Account!!!")
+    #     # if ac_number == Account.number:
+    #     #     Account.amount += amount
+    #     # else:
+    #     #     print("Invalid Account!!!")
+    #     # if ac_number :
+    #     #     self.accounts.amount += amount
+    #     # else:
+    #     #     print("invalid Account!!")
+    #     # for account in self.accounts:
+    #     #     if account.number == ac_number:
+    #     #         account.amount += amount
+    #     #         new_balance = account.amount
+    #     #     else:
+    #     #         print("Invalid Account Number!!")
+    #     # return new_balance
+    # def make_withdrawal(self,ac_number,amount):
+    #     account = session.query(Account).filter_by(number=ac_number).first()
+    #     if account in self.accounts:
+    #         if amount > 0 and  amount < account.amount:
+    #             account.amount -= amount
+    #         else:
+    #             print("Withdrawal cannot be less than 0 or  more than the remaining balance")
+    #     else:
+    #         print("Invalid Account!!!")
         
     def __repr__(self):
         return f" Customer Name: {self.name}"      
