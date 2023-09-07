67d971c5094f2b253a51f417ce88aa0cc49c954e
Wandati <Petersimon801@gmail.com>
2023-09-07 21:25:53 +0300

    edit

--- models/associations.py
+++ models/associations.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Table,Column,ForeignKey,Integer
-# from sqlalchemy.orm import declarative_base
 from .base import Base
-# Base = declarative_base()
 customer_bank_association = Table(
     'customer_bank_association',
     Base.metadata,
