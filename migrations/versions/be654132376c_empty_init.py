6a857c1c50684910022493c70db3f1fcdaa094cc
Wandati <Petersimon801@gmail.com>
2023-09-07 21:25:36 +0300

    empty init

---
+++ migrations/versions/be654132376c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty init
+
+Revision ID: be654132376c
+Revises: 
+Create Date: 2023-09-07 20:40:57.685984
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'be654132376c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
